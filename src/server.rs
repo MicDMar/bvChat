4f3e6f4b37c31e5d591827f339d80d2f0368af03
Justin Rubek <rubejus@bvu.edu>
2019-04-09 20:23:30 -0500

    Added tests for get_admin and is_admin

--- src/server.rs
+++ src/server.rs
@@ -155,9 +155,8 @@ macro_rules! broadcast {
         let message = format!($($arg)*);
         let bytes = message.as_bytes();
         $list.iter_mut().for_each(|(_key, val)| {
-            val.socket.write(bytes).expect("Failed to send");
+            val.socket.write(bytes);
         });
-
     }
 }
 
@@ -269,4 +268,52 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{UserData, UserList};
+    use std::collections::HashMap;
+    use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream};
+    use std::thread;
+
+    macro_rules! t {
+        ($e:expr) => {
+            match $e {
+                Ok(t) => t,
+                Err(e) => panic!("received error for `{}`: {}", stringify!($e), e),
+            }
+        }
+    }
+
+    #[test]
+    fn get_admin() {
+        let a = Ipv4Addr::new(127, 0, 0, 1);
+        let p = 12345;
+        let e = SocketAddr::V4(SocketAddrV4::new(a, p));
+            
+        let listener = t!(TcpListener::bind(&e));
+
+        thread::spawn(move || {
+            let mut list: UserList = HashMap::new();
+            list.insert(String::from("test") , UserData { socket: TcpStream::connect(&e).unwrap() , user_id: 0 });
+            assert_eq!(super::get_admin(&list), Some(String::from("test")));
+        });
+    }
+
+    #[test]
+    fn is_admin() {
+        let a = Ipv4Addr::new(127, 0, 0, 1);
+        let p = 12346;
+        let e = SocketAddr::V4(SocketAddrV4::new(a, p));
+            
+        let listener = t!(TcpListener::bind(&e));
+
+        thread::spawn(move || {
+            let mut list: UserList = HashMap::new();
+            list.insert(String::from("test") , UserData { socket: TcpStream::connect(&e).unwrap() , user_id: 0 });
+            assert!(super::is_admin("test", &list));
+        });
+        
+    }
 }
